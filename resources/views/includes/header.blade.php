ed0062dccf309910f91ab5ed81ff5f2b2cb7edbb
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-22 08:46:14 +0200

    Top Header section complete

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,26 +1,25 @@
 <header>
-    <div class="container">
+    {{-- TOP HEADER --}}
+    <div id="top-header">
+      <div class="container">
         <div class="row">
-            <div class="col">
-                <nav class="navbar navbar-expand-lg bg-light">
-                    <div class="container-fluid">
-                      <a class="navbar-brand" href="{{ route('index') }}">Home</a>
-                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                      </button>
-                      <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                          <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Comics</a>
-                          </li>
-                          <li class="nav-item">
-                            <a class="nav-link" href="{{ route('comics.create') }}">New Comics</a>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                </nav>
+          <div class="col">
+            <div class="d-flex justify-content-end align-items-center my-1">
+              <p class="text-white m-0 me-5">DC POWER VISA</p>
+              <p class="text-white m-0">ADDITIONAL DC SITES</p>
             </div>
+          </div>
         </div>
+      </div>
+    </div>
+    {{-- BOTTOM HEADER --}}
+    <div id="bottom-header">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+
+          </div>
+        </div>
+      </div>
     </div>
 </header>
