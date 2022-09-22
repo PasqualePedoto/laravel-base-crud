e8afec3517ac89c1496b4e32c42a74b5e4dafcab
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-22 15:11:24 +0200

    Create index.blade.php in comics folder

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,12 +6,19 @@
 
 {{-- Main Content --}}
 @section('main-content')
-    <main class="bg-dark">
+    <main class="bg-dark text-white">
         <div class="container">
             <div class="row flex-wrap">
-                <div class="col">
-                    ciao
-                </div>
+                @forelse($comics as $comic)
+                    <div class="col my-4">
+                        <figure class="m-0">
+                            <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}" class="img-fluid">
+                        </figure>
+                        <div class="d-flex align-items-center">{{ $comic->title }}</div>
+                    </div>
+                @empty
+                    <p>Non ci sono comics</p>
+                @endforelse
             </div>
         </div>
     </main>
