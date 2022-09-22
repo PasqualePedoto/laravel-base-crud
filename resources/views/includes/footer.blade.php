03b2eab92e887b4d55ff5a3ac7490884bf7b13b9
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-22 08:31:21 +0200

    Footer complete

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,141 @@
+<footer class="position-fixed bottom-0 w-100">
+    <div class="position-relative">
+        <div class="container">
+            <div class="row">
+                {{-- Footer links --}}
+                <div class="col-6">
+                    <div class="d-flex flex-wrap align-content-center align-items-center h-100">
+                        <div class="d-flex w-100">
+                            <div class="me-4">
+                                <h6 class="text-white">DC COMICS</h6>
+                                <ul>
+                                    <li>
+                                        <a href="#">Characters</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Comics</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Movies</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">TV</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Games</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Videos</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">News</a>
+                                    </li>
+                                </ul>
+                                <h6  class="text-white">SHOP</h6>
+                                <ul>
+                                    <li>
+                                        <a href="#">Shop DC</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Shop DC Collectibles</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="me-4">
+                                <h6>DC</h6>
+                                <ul>
+                                    <li>
+                                        <a href="#">Terms of Use</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Privacy Policy (New)</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Ad Choices</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Advertising</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Jobs</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Subscriptions</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Talent Workshops</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">CPSC Certificates</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Ratings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Shop Help</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Contact Us</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="me-4">
+                                <h6>SITES</h6>
+                                <ul>
+                                    <li>
+                                        <a href="#">DC</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">MAD Magazine</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Kids</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Universe</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Power Visa</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        {{-- Cockies settings --}}
+                        <div class="cockies"> 
+                            <p>
+                                All Site Content TM and 2020 DC Entertainment, 
+                                unless otherwise <a href="#">noted here</a>. All
+                                rights reserved. <a href="#">Cockies Settings</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                {{-- DC Logo --}}
+                <div class="col-6">
+                    <figure class="m-0">
+                        <img src="{{ asset('images/dc-logo-bg.png') }}" alt="{{ asset('images/dc-logo-bg.png') }}" class="img-fluid">
+                    </figure>
+                </div>
+            </div>
+        </div>
+        <div id="bottom-footer" class="position-absolute bottom-0 right-0 left-0 w-100">
+            <div class="container">
+                <div class="d-flex justify-content-between align-items-center py-4">
+                    {{-- Button --}}
+                    <div>
+                        <button class="p-2 text-white">SIGN-UP NOW!</button>
+                    </div>
+                    {{-- Social links --}}
+                    <div class="d-flex align-items-center justify-content-center">
+                        <div class="follow-us me-3">FOLLOW US</div>
+                        <figure class="m-0 me-3"><img class="img-fluid" src="{{ asset('images/footer-facebook.png') }}" alt=""></figure>
+                        <figure class="m-0 me-3"><img class="img-fluid" src="{{ asset('images/footer-periscope.png') }}" alt=""></figure>
+                        <figure class="m-0 me-3"><img class="img-fluid" src="{{ asset('images/footer-pinterest.png') }}" alt=""></figure>
+                        <figure class="m-0 me-3"><img class="img-fluid" src="{{ asset('images/footer-twitter.png') }}" alt=""></figure>
+                        <figure class="m-0 me-3"><img class="img-fluid" src="{{ asset('images/footer-youtube.png') }}" alt=""></figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
