f0a16549e1b5ba4e2aa12daf9d1354adf9d94cbc
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-22 09:16:06 +0200

    Added active effect on navbar links in Header section

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -17,7 +17,55 @@
       <div class="container">
         <div class="row">
           <div class="col">
-
+            <div class="d-flex justify-content-between py-3">
+              {{-- LOGO DC --}}
+              <figure class="m-0">
+                <img src="{{ asset('images/dc-logo.png') }}" alt="logo-dc" class="img-fluid">
+              </figure>
+              {{-- NAVBAR --}}
+              <div>
+                <nav class="h-100">
+                  <ul class="m-0 h-100 d-flex align-items-center-justify-content-center">
+                    <li class="active">
+                      <a href="#">CHARACTERS</a>
+                    </li>
+                    <li>
+                      <a href="#">COMICS</a>
+                    </li>
+                    <li>
+                      <a href="#">MOVIES</a>
+                    </li>
+                    <li>
+                      <a href="#">TV</a>
+                    </li>
+                    <li>
+                      <a href="#">GAMES</a>
+                    </li>
+                    <li>
+                      <a href="#">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                      <a href="#">VIDEOS</a>
+                    </li>
+                    <li>
+                      <a href="#">FANS</a>
+                    </li>
+                    <li>
+                      <a href="#">NEWS</a>
+                    </li>
+                    <li>
+                      <a href="#">SHOP</a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+              {{-- SEARCH --}}
+              <div class="d-flex align-items-center">
+                <div class="d-flex align-item-center justify-content-center">
+                  <input type="search" placeholder="Search..">
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
